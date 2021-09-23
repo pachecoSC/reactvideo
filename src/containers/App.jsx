b5bb1eecf6edd1046ef718fafbc0de1c5fc75e0c
Christian Pacheco Sarango <pachecoschris@gmail.com>
2021-09-22 22:07:04 -0500

    se agrego un config con la url de la api.. se consume un servicio para obtener los datos de la api usando useEffect y useState

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+import config from '../../config'
 import '../assets/styles/App.scss'
 
 import Header from '../components/Header'
@@ -9,34 +10,55 @@ import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Categories tittle='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories tittle='Tendencia'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories tittle='Originales de video'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-)
+// const API = 'http://localhost:3000/initialState';
+
+const App = () => {
+  const [videos, setVideos] = useState([])
+  //  ! useState necesita parametros
+  //  ! string      'texto'
+  //  ! int      0
+  //  ! objeto      []
+
+  useEffect(() => {
+    fetch(config.API_URL)
+      .then((response) => response.json())
+      .then((data) => setVideos(data))
+  }, [])
+  //  ! useEffect recibe dos parametros
+  //  ! 1. - funcion a trabajar.
+  //  ! 2. - cuando se va a ejecutar, si no enviamos parametro seria un bucle infinito,si tiene un array vacio
+  //  ! [] esta función solo se ejecutará al montar o desmontar el componente.
+
+  console.log(videos)
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      <Categories tittle='Mi lista'>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Categories tittle='Tendencia'>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Categories tittle='Originales de video'>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  )
+}
 
 export default App
