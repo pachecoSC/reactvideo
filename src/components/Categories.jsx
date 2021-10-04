b60808c6b49d623a74a4416fc33f05f1b38ddeac
Christian Pacheco Sarango <pachecoschris@gmail.com>
2021-10-03 19:47:02 -0500

    se agrego funcionalida de creacion del store redux

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../assets/styles/components/Categories.scss'
 import PropTypes from 'prop-types'
 
-const Categories = ({ children, tittle }) => (
+const Categories = ({ children, tittle, visible }) => visible && (
   <>
     <h3 className='categories__title'>{tittle}</h3>
     {children}
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
-
-import config from '../../config'
+import { connect } from 'react-redux'
+// import config from '../../config'
 import '../assets/styles/App.scss'
 
 import Header from '../components/Header'
@@ -10,61 +10,81 @@ import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
-import useInitialState from '../hooks/useInitialState'
+// import useInitialState from '../hooks/useInitialState'
 // const API = 'http://localhost:3000/initialState';  //se trae en el componente config
 
-const Home = () => {
-  const initialState = useInitialState(config.API_URL)
+const Home = ({ myList, trends, originals }) => {
+  // const initialState = useInitialState(config.API_URL)
+  // eslint-disable-next-line no-use-before-define
+  const categoriesTittle = Array.from(Object.keys(mapStateToProps(''))) //obtiene un array desde un objeto
+  // console.log(categoriesTittle)
+  //funcion switch
+  function SelectionCategory(key) {
+    const res = { tittle: '', visible: '', componente: '' }
+    switch (key) {
+      case 'myList':
+        res.tittle = 'mi lista'
+        res.visible = myList?.length > 0
+        res.componente = myList?.map((item) => <CarouselItem key={item.id} {...item} />)
+        break
+      case 'trends':
+        res.tittle = 'tendencias'
+        res.visible = trends?.length > 0
+        res.componente = trends?.map((item) => <CarouselItem key={item.id} {...item} />)
+        break
+      case 'originals':
+        res.tittle = 'originales'
+        res.visible = originals?.length > 0
+        res.componente = originals?.map((item) => <CarouselItem key={item.id} {...item} />)
+        break
+      default:
+        break
+    }
+    return res
+  }
 
-  // console.log(initialState)
+  //funcion para obtner el titulo de la categoria
+  function fGetTittleCategory(key) {
+    const res = SelectionCategory(key)
+    return res.tittle
+  }
+  //funcion para saber si es la categoria seria visible o no
+  function fVisibleCategoriy(key) {
+    const res = SelectionCategory(key)
+    // console.log('visible', res.visible)
+    return res.visible
+  }
+  //funcion para pintar segun la categoria
+  function fBindeoCarousel(key) {
+    const res = SelectionCategory(key)
+    return res.componente
+  }
+
+  // ! commit 461f6787eacb40441264af10fe2c3c993a50cbd2 -- codigo funciona con el hook useInitialState
   return (
     <div className='App'>
       <Header dest='home' />
       <Search />
-      {initialState &&
-        Object.keys(initialState).map(
-          (cat) => (
-            initialState[cat].length > 0 && (
-              <Categories tittle={cat} key={cat}>
-                <Carousel>
-                  {initialState[cat].map((item) => (
-                    <CarouselItem key={item.id} {...item} />
-                  ))}
-                </Carousel>
-              </Categories>
-            )),
-        )}
+      {categoriesTittle.map(
+        (cat) => categoriesTittle.length > 0 && (
+          <Categories tittle={fGetTittleCategory(cat)} key={cat} visible={fVisibleCategoriy(cat)}>
+            <Carousel>
+              {fBindeoCarousel(cat)}
+            </Carousel>
+          </Categories>
+        ),
+      )}
       <Footer />
     </div>
   )
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  }
+}
 
-//  ! ejemplo de bindeo de las categorias
-// {initialState.mylist?.length > 0 && (
-//   <Categories tittle='Mi lista'>
-//     <Carousel>
-//       <CarouselItem />
-//     </Carousel>
-//   </Categories>
-// )}
-// {initialState.trends?.length > 0 && ( // pregunta si tiene datos. si-muestra, no-oculta
-//   <Categories tittle='Tendencia'>
-//     <Carousel>
-//       {/* bindear carouselItem */}
-//       {initialState.trends?.map((item) => (
-//         <CarouselItem key={item.id} {...item} />
-//       ))}
-//     </Carousel>
-//   </Categories>
-// )}
-// {initialState.originals?.length > 0 && (
-//   <Categories tittle='Originales de video'>
-//     <Carousel>
-//       {initialState.originals?.map((item) => (
-//         <CarouselItem key={item.id} {...item} />
-//       ))}
-//     </Carousel>
-//   </Categories>
-// )}
+export default connect(mapStateToProps, null)(Home)
