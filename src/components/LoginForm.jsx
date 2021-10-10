9fcfcd6aea8f5c6cfc4d70627a2466c67e9289a5
Christian Pacheco Sarango <pachecoschris@gmail.com>
2021-10-10 18:02:54 -0500

    se ajusto modo de trabajar con los estados de los inputs

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,44 +1,77 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../assets/styles/components/LoginForm.scss'
 //importacion de imagenes
 import GoogleIcon from '../assets/images/google-icon.png'
 import TwitterIcon from '../assets/images/twitter-icon.png'
 
-const LoginForm = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Inicia sesión</h2>
-      <form className='login__container--form'>
-        <input className='input' type='text' placeholder='Correo' />
-        <input className='input' type='password' placeholder='Contraseña' />
-        <button type='button' className='button'>Iniciar sesión</button>
-        <div className='login__container--remember-me'>
-          <label>
-            <input type='checkbox' id='cbox1' value='first_checkbox' />
-            Recuérdame
-          </label>
-          <Link to='/'>Olvidé mi contraseña</Link>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img src={GoogleIcon} alt='google' />
-          {' '}
-          Inicia sesión con Google
-        </div>
-        <div>
-          <img src={TwitterIcon} alt='twitter' />
-          Inicia sesión con Twitter
-        </div>
+const LoginForm = () => {
+  const [form, setValues] = useState({
+    email: '',
+  })
+
+  const handleInput = (e) => {
+    // console.log(e.target.value)
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const submitLoginButton = (e) => {
+    e.preventDefault() // evita que se envien los parametros por la url
+    console.log(form)
+  }
+
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia sesión</h2>
+        <form className='login__container--form' onSubmit={submitLoginButton}>
+          <input
+            name='email'
+            className='input'
+            type='text'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>
+            Iniciar sesión
+          </button>
+          <div className='login__container--remember-me'>
+            <label>
+              <input type='checkbox' id='cbox1' value='first_checkbox' />
+              Recuérdame
+            </label>
+            <Link to='/'>Olvidé mi contraseña</Link>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={GoogleIcon} alt='google' />
+            {' '}
+            Inicia sesión con Google
+          </div>
+          <div>
+            <img src={TwitterIcon} alt='twitter' />
+            Inicia sesión con Twitter
+          </div>
+        </section>
+        <p className='login__container--register'>
+          No tienes ninguna cuenta
+          <Link to='/register'> Regístrate</Link>
+        </p>
       </section>
-      <p className='login__container--register'>
-        No tienes ninguna cuenta
-        <Link to='/register'> Regístrate</Link>
-      </p>
     </section>
-  </section>
-)
+  )
+}
 
 export default LoginForm
